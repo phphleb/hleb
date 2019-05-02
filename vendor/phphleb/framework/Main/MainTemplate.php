ca179cf8ca8141638ec8f9d6435f515d5479687c
Foma Tuturov <fomiash@yandex.ru>
2019-05-02 16:58:00 +0300

    extr skip

--- vendor/phphleb/framework/Main/MainTemplate.php
+++ vendor/phphleb/framework/Main/MainTemplate.php
@@ -8,7 +8,7 @@ class MainTemplate
     {
         Info::insert("Templates", trim($_hl_template_x_, "/"));
 
-        if(count($_hl_template_params_x_)) extract ($_hl_template_params_x_, EXTR_OVERWRITE);
+        if(count($_hl_template_params_x_)) extract ($_hl_template_params_x_, EXTR_SKIP);
 
         // Create HLEB Template.
         require HLEB_GLOBAL_DIRECTORY . "/resources/views/" . trim($_hl_template_x_, "/") . ".php";
