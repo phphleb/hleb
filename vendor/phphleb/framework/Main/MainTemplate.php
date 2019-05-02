154c28853cc29a8779a754a210ac01186a7f30f2
Foma Tuturov <fomiash@yandex.ru>
2019-05-02 13:52:04 +0300

    upd template

--- vendor/phphleb/framework/Main/MainTemplate.php
+++ vendor/phphleb/framework/Main/MainTemplate.php
@@ -6,12 +6,12 @@ class MainTemplate
 {
     function __construct(string $_hl_template_x_, array $_hl_template_params_x_)
     {
-        if(count($_hl_template_params_x_)) extract ($_hl_template_params_x_);
+        Info::insert("Templates", trim($_hl_template_x_, "/"));
+
+        if(count($_hl_template_params_x_)) extract ($_hl_template_params_x_, EXTR_OVERWRITE);
 
         // Create HLEB Template.
         require HLEB_GLOBAL_DIRECTORY . "/resources/views/" . trim($_hl_template_x_, "/") . ".php";
-
-        Info::insert("Templates", trim($_hl_template_x_, "/"));
     }
 
 }
