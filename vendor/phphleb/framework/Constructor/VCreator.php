efafe436fffc4730b51d2dd7d082ddea4de36233
Foma Tuturov <fomiash@yandex.ru>
2019-05-02 20:29:26 +0300

    add vcreator

---
+++ vendor/phphleb/framework/Constructor/VCreator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Hleb\Constructor;
+
+class VCreator
+{
+    private $includeFile = "";
+
+    function __construct(string $include)
+   {
+       $this->includeFile = $include;
+   }
+
+   public function view()
+   {
+       extract (hleb_to0me1cd6vo7gd_data());
+
+      require $this->includeFile;
+
+   }
+}
+
