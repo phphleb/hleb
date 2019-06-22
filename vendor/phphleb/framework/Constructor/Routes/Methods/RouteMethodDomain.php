61d147c6d8e2e736444aff58e23c29feebac7e2f
Foma Tuturov <fomiash@yandex.ru>
2019-06-22 19:35:59 +0300

    route domain

---
+++ vendor/phphleb/framework/Constructor/Routes/Methods/RouteMethodDomain.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Hleb\Constructor\Routes\Methods;
+
+use Hleb\Scheme\Home\Constructor\Routes\{
+    StandardRoute
+};
+use Hleb\Constructor\Routes\MainRouteMethod;
+use Hleb\Main\Errors\ErrorOutput;
+
+class RouteMethodDomain extends MainRouteMethod
+{
+
+    protected $instance;
+
+    /**
+     * RouteMethodName constructor.
+     * @param StandardRoute $instance
+     * @param array|string $name
+     * @param int $level
+     * @param bool $pattern
+     */
+    function __construct(StandardRoute $instance, $name, $level, $pattern)
+    {
+        $this->method_type_name = "domain";
+
+        $this->instance = $instance;
+
+        $this->calc([is_array($name) ? $name : [$name], $level, $pattern]);
+
+    }
+
+
+    private function calc($params)
+    {
+
+        $this->domain = $params;
+
+    }
+
+
+}
