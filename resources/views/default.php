55aaff48afe133d324fcbb97fdc0998d07fe3e98
Foma Tuturov <fomiash@yandex.ru>
2019-08-09 21:43:52 +0300

    add const

--- resources/views/default.php
+++ resources/views/default.php
@@ -3,6 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width" />
+    <link rel="icon" href= "//<?= HLEB_MAIN_DOMAIN;  ?>/favicon.ico" type="image/x-icon">
     <style>
 
         body{
