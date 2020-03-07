b978494b73d46ef74861c8cc1927714ece5209f6
Foma Tuturov <fomiash@yandex.ru>
2020-03-07 14:13:22 +0300

    new version

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ define('HLEB_START', microtime(true));
 
 define('HLEB_PUBLIC_DIR', __DIR__);
 
-define('HLEB_FRAME_VERSION', "1.4.1");
+define('HLEB_FRAME_VERSION', "1.5.0");
 
 // Initialization.
 // Инициализация.
