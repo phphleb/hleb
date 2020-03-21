0ba751af57116f1d166f20d7245260d7af9d15fc
Foma Tuturov <fomiash@yandex.ru>
2020-03-21 16:50:32 +0300

    upd page

--- resources/views/default.php
+++ resources/views/default.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width" />
     <link rel="icon" href= "//<?= HLEB_MAIN_DOMAIN;  ?>/favicon.ico" type="image/x-icon">
     <style>
-
-        body{
+        html, body{
             padding:0;
             margin:0;
             width:100%;
@@ -26,10 +25,16 @@
             display:block;
             margin-bottom: 30px;
             color: #FF786C;
-            margin-left: 10px;
         }
-        .hl-link{
-            padding-left: 20px;
+        a.hl-block{
+            width:max-content;
+            margin-right: 15px;
+        }
+        img.hl-block{
+            margin-right: 40px;
+        }
+        div.hl-block{
+            margin-left: 10px;
         }
 
     </style>
@@ -38,7 +43,7 @@
 <body>
     <div id="hl-cont" align="center">
         <img src="/svg/logo.svg" width="200" height="200" class="hl-block" alt="HL">
-        <a href="https://github.com/phphleb/hleb/blob/master/readme.md"  target="_blank" class="hl-block hl-link">Instruction for use</a>
+        <a href="https://github.com/phphleb/hleb/blob/master/readme.md"  target="_blank" class="hl-block">Instruction for use</a>
     </div>
     <br>
     <div class="hl-block">v<?= HLEB_PROJECT_VERSION ?></div>
