7b4e64a0f2e9f90f6913a2401ff783eec0907096
Foma Tuturov <fomiash@yandex.ru>
2020-07-08 13:38:16 +0300

    fix route

--- vendor/phphleb/framework/Constructor/Cache/CacheRoutes.php
+++ vendor/phphleb/framework/Constructor/Cache/CacheRoutes.php
@@ -65,12 +65,11 @@ class CacheRoutes
     private function createRoutes()
     {
         print hl_print_fulfillment_inspector(HLEB_LOAD_ROUTES_DIRECTORY , '/main.php');
-        Route::end();
-
         // Reserved Name
         if(file_exists(HLEB_LOAD_ROUTES_DIRECTORY . '/hlogin/reg.php')){
             print hl_print_fulfillment_inspector(HLEB_LOAD_ROUTES_DIRECTORY , '/hlogin/reg.php');
         }
+        Route::end();
     }
 
     private function getFpmUserName()
