1a43c02eca6bbe595b7d80a0e1f6035f192117fd
Foma Tuturov <fomiash@yandex.ru>
2020-10-21 21:51:26 +0300

    new version

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 // Все вызовы направляются в этот файл.
 
 define('HLEB_START', microtime(true));
-define('HLEB_FRAME_VERSION', "1.5.30");
+define('HLEB_FRAME_VERSION', "1.5.31");
 define('HLEB_PUBLIC_DIR', __DIR__);
 
 
@@ -19,15 +19,6 @@ header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: SAMEORIGIN");
 // ...
-
-// Additional structural paths
-// Дополнительные структурные пути
-/*
-define('HLEB_GLOBAL_DIRECTORY', realpath(__DIR__ . '/../'));
-define('HLEB_SEARCH_START_CONFIG_FILE', HLEB_GLOBAL_DIRECTORY);
-define('HLEB_SEARCH_DBASE_CONFIG_FILE', realpath(HLEB_GLOBAL_DIRECTORY . '/database'));
-define('HLEB_STORAGE_DIRECTORY', realpath(HLEB_GLOBAL_DIRECTORY . '/storage'));
-*/
 
 // Initialization.
 // Инициализация.
