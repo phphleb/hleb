e549d500e7202831cc61ac81b998536b988266ce
Foma Tuturov <fomiash@yandex.ru>
2020-12-27 04:29:38 +0300

    upd twig

--- vendor/phphleb/framework/Constructor/TwigCreator.php
+++ vendor/phphleb/framework/Constructor/TwigCreator.php
@@ -13,17 +13,21 @@ namespace Hleb\Constructor;
 class TwigCreator
 {
     public function view(string $path) {
-        $loader = new \Twig\Loader\FilesystemLoader(HL_TWIG_LOADER_FILESYSTEM);
-        $twig = new \Twig\Environment($loader, array(
-            'cache' => HL_TWIG_CACHED,
-            'debug' => HLEB_PROJECT_DEBUG,
-            'charset' => HL_TWIG_CHARSET,
-            'auto_reload' => HL_TWIG_AUTO_RELOAD,
-            'strict_variables' => HL_TWIG_STRICT_VARIABLES,
-            'autoescape' => HL_TWIG_AUTOESCAPE,
-            'optimizations' => HL_TWIG_OPTIMIZATIONS
-        ));
-        echo $twig->render($path, hleb_to0me1cd6vo7gd_data());
+        if(class_exists('Twig\Loader\FilesystemLoader') && class_exists('Twig\Environment')) {
+            $loader = new \Twig\Loader\FilesystemLoader(HL_TWIG_LOADER_FILESYSTEM);
+            $twig = new \Twig\Environment($loader, array(
+                'cache' => HL_TWIG_CACHED,
+                'debug' => HLEB_PROJECT_DEBUG,
+                'charset' => HL_TWIG_CHARSET,
+                'auto_reload' => HL_TWIG_AUTO_RELOAD,
+                'strict_variables' => HL_TWIG_STRICT_VARIABLES,
+                'autoescape' => HL_TWIG_AUTOESCAPE,
+                'optimizations' => HL_TWIG_OPTIMIZATIONS
+            ));
+            echo $twig->render($path, hleb_to0me1cd6vo7gd_data());
+        } else {
+            error_log('Undefined Twig class on TwigCreator');
+        }
     }
 }
 
