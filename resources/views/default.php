9cf13804031c16e857c9b708e14de3cb00d041ee
Foma Tuturov <fomiash@yandex.ru>
2021-09-12 23:31:01 +0300

    new default page

--- resources/views/default.php
+++ resources/views/default.php
@@ -21,6 +21,12 @@
             top: 20%;
             width: 100%;
         }
+        div#hl-cont-register{
+            position:absolute;
+            left:0;
+            top: 100px;
+            width: 100%;
+        }
         .hl-block{
             display:block;
             margin-bottom: 30px;
@@ -36,16 +42,53 @@
         div.hl-block{
             margin-left: 10px;
         }
+        .reg-type{
+            color: #637496;
+        }
     </style>
     <title>HLEB Start Page</title>
 </head>
 <body>
-    <div id="hl-cont" align="center">
+<?php if (class_exists('App\Controllers\Hlogin\AuthController') && class_exists('Phphleb\Hlogin\App\OriginData')): ?>
+    <div id="hl-cont-register" align="center">
         <img src="/svg/logo.svg" width="200" height="200" class="hl-block" alt="HL">
-        <a href="https://github.com/phphleb/hleb/blob/master/readme.md"  target="_blank" rel="noreferrer" class="hl-block">Instruction for use</a>
+        <a href="https://github.com/phphleb/hleb/blob/master/readme.md" target="_blank" rel="noreferrer" class="hl-block">Manual for HLEB framework</a>
+        <br><br>
+        <div>
+            <img src="/en/login/resource/<?= Phphleb\Hlogin\App\Main::getVersion() ?>/all/svg/svg/hlogin-logo/" width="170" height="70"><br>
+            <span class="reg-type"><a href="/en/login/profile/" id="hlLink" class="reg-type">Registration panel</a> in </span>
+            <select onchange="document.getElementById('hlLink').href = '/' + this.value + '/login/profile/'">
+                <?php
+                foreach (Phphleb\Hlogin\App\OriginData::LANGUAGES as $lang) {
+                    echo "<option " . ($lang == 'en' ? "selected" : "") . " value=\"{$lang}\">{$lang}</option>";
+                }
+                ?>
+            </select>
+            <br><br>
+            <span class="reg-type"> Show panel </span>
+            <select onchange="hloginSetDesignToPopups(this.value)">
+                <?php
+                $type = Phphleb\Hlogin\App\OriginData::getDesign();
+                foreach (Phphleb\Hlogin\App\OriginData::activeTypes() as $design) {
+                    echo "<option " . ($design === $type ? "selected" : "") . " value=\"{$design}\">{$design}</option>";
+                }
+                ?>
+            </select>
+            <span class="reg-type"> type </span>
+        </div>
     </div>
     <br>
-    <div class="hl-block">v<?= HLEB_PROJECT_VERSION ?></div>
+    <?php
+    // Adding registration buttons / Добавление кнопок регистрации
+    includeTemplate('hlogin/templates/add');
+else: ?>
+    <div id="hl-cont" align="center">
+        <img src="/svg/logo.svg" width="200" height="200" class="hl-block" alt="HL">
+        <a href="https://github.com/phphleb/hleb/blob/master/readme.md" target="_blank" rel="noreferrer" class="hl-block">Instructions for use</a>
+    </div>
+<?php endif; ?>
+</div>
+<br>
+<div class="hl-block">v<?= HLEB_PROJECT_VERSION ?></div>
 </body>
 </html>
-
