fcb2d45c100c8edb56b3fd2e5e11f3903131d8d9
Foma Tuturov <fomiash@yandex.ru>
2021-10-22 13:08:16 +0300

    new version

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 // Все вызовы направляются в этот файл.
 
 define('HLEB_START', microtime(true));
-define('HLEB_FRAME_VERSION', "1.5.71");
+define('HLEB_FRAME_VERSION', "1.5.72");
 define('HLEB_PUBLIC_DIR', __DIR__);
 
 
--- vendor/phphleb/framework/init.php
+++ vendor/phphleb/framework/init.php
@@ -1,6 +1,6 @@
 <?php
 
-define('HLEB_PROJECT_FULL_VERSION', '1.5.71');
+define('HLEB_PROJECT_FULL_VERSION', '1.5.72');
 
 require HLEB_PROJECT_DIRECTORY . '/Scheme/App/Controllers/MainController.php';
 
