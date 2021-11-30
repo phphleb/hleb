f98f912d58a85a2c19e77e80c5bbc0188fca779b
Foma Tuturov <fomiash@yandex.ru>
2021-12-01 01:35:00 +0300

    upd db config

--- database/default.dbase.config.php
+++ database/default.dbase.config.php
@@ -7,39 +7,48 @@
  * Этот файл необходимо скрывать при копировании проекта, так как он содержит важную информацию.
  */
 
-define("HLEB_TYPE_DB", "mysql.myname");
+define('HLEB_TYPE_DB', 'mysql.myname');
 
-define("HLEB_PARAMETERS_FOR_DB", [
+define('HLEB_MUTEX_TYPE_REDIS', 'redis.predis');
 
-    "mysql.myname" => [
-        "mysql:host=localhost",
-        "port=3360",
-        "dbname=databasename",
-        "charset=utf8",
-        "user" => "username",
-        "pass" => "password"
+define('HLEB_PARAMETERS_FOR_DB', [
+
+    'mysql.myname' => [
+        'mysql:host=localhost',
+        'port=3360',
+        'dbname=maintesthleb',
+        'charset=utf8',
+        'user' => 'newuser',
+        'pass' => 'password'
     ],
 
-    "sqlite.myname" => [
-        "sqlite:c:/main.db",
-        "user" => "username",
-        "pass" => "password"
+    'sqlite.myname' => [
+        'sqlite:c:/main.db',
+        'user' => 'username',
+        'pass' => 'password'
     ],
 
-    "postgresql.myname" => [
-        "pgsql:host=127.0.0.1",
-        "port=5432",
-        "dbname=databasename",
-        "user" => "username",
-        "pass" => "password"
+    'postgresql.myname' => [
+        'pgsql:host=127.0.0.1',
+        'port=5432',
+        'dbname=databasename',
+        'user' => 'username',
+        'pass' => 'password'
     ],
 
-    "mysql.sphinx-search" => [
-        "mysql:host=127.0.0.1",
-        "port=9306",
-        "user" => "username",
-        "pass" => "password"
+    'mysql.sphinx-search' => [
+        'mysql:host=127.0.0.1',
+        'port=9306',
+        'user' => 'username',
+        'pass' => 'password'
     ],
+
+    'redis.predis' => [
+        'scheme' => 'tcp',
+        'host' => '127.0.0.1',
+        'port' => '6379',
+        'password' => 'password'
+    ]
 
 ]);
 
