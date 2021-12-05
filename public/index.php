605a2b37bce34ac92dd21997e84ae9fa567d4f06
Foma Tuturov <fomiash@yandex.ru>
2021-12-05 11:47:59 +0300

    new version

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 // Все вызовы направляются в этот файл.
 
 define('HLEB_START', microtime(true));
-define('HLEB_FRAME_VERSION', "1.5.78");
+define('HLEB_FRAME_VERSION', "1.5.79");
 define('HLEB_PUBLIC_DIR', __DIR__);
 
 
