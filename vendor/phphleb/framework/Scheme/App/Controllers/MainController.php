bf1cd2e4e9ef3d53563e932ae40a4be17afbd05e
Foma Tuturov <fomiash@yandex.ru>
2022-07-29 21:56:03 +0300

    controller data

--- vendor/phphleb/framework/Scheme/App/Controllers/MainController.php
+++ vendor/phphleb/framework/Scheme/App/Controllers/MainController.php
@@ -2,8 +2,18 @@
 
 namespace Hleb\Scheme\App\Controllers;
 
-class MainController
+class MainController extends BaseController
 {
-    function __construct() {}
+    /**
+     * Parameter to use only for class testing.
+     *
+     * Параметр использовать только для тестирования класса.
+     *
+     * @param array|null $data
+     */
+    function __construct(array $data = null)
+    {
+        parent::__construct($data);
+    }
 }
 
--- vendor/phphleb/framework/Scheme/App/Middleware/MainMiddleware.php
+++ vendor/phphleb/framework/Scheme/App/Middleware/MainMiddleware.php
@@ -2,8 +2,20 @@
 
 namespace Hleb\Scheme\App\Middleware;
 
-class MainMiddleware
+use Hleb\Scheme\App\Controllers\BaseController;
+
+class MainMiddleware extends BaseController
 {
-    function __construct() {}
+    /**
+     * Parameter to use only for class testing.
+     *
+     * Параметр использовать только для тестирования класса.
+     *
+     * @param array|null $data
+     */
+    function __construct(array $data = null)
+    {
+        parent::__construct($data);
+    }
 }
 
