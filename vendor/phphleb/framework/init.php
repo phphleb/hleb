d66029e1154a40cffd6f0cfbf5fe86e3e6a7c89b
Foma Tuturov <fomiash@yandex.ru>
2022-08-29 15:13:59 +0300

    new version

--- vendor/phphleb/framework/init.php
+++ vendor/phphleb/framework/init.php
@@ -1,6 +1,6 @@
 <?php
 
-define('HLEB_PROJECT_FULL_VERSION', '1.6.70');
+define('HLEB_PROJECT_FULL_VERSION', '1.6.71');
 
 require HLEB_PROJECT_DIRECTORY . '/Scheme/App/Controllers/MainController.php';
 
