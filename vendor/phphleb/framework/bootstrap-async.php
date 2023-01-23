8cca0199ca8d5e154b366dbce67e48c799bcdfb0
Foma Tuturov <fomiash@yandex.ru>
2023-01-23 23:05:37 +0300

    upd const

--- vendor/phphleb/framework/bootstrap-async.php
+++ vendor/phphleb/framework/bootstrap-async.php
@@ -16,7 +16,7 @@ defined('HLEB_START') or define('HLEB_START', microtime(true));
 $GLOBALS['HLEB_START'] = microtime(true);
 
 //To set a different directory name 'vendor' add HLEB_VENDOR_DIR_NAME to the constants
-define('HLEB_VENDOR_DIRECTORY', defined('HLEB_VENDOR_DIR_NAME') ? HLEB_GLOBAL_DIRECTORY . '/' . HLEB_VENDOR_DIR_NAME : dirname(__DIR__, 2));
+defined('HLEB_VENDOR_DIRECTORY') or define('HLEB_VENDOR_DIRECTORY', defined('HLEB_VENDOR_DIR_NAME') ? HLEB_GLOBAL_DIRECTORY . '/' . HLEB_VENDOR_DIR_NAME : dirname(__DIR__, 2));
 
 // End of script execution (before starting the main project).
 if (!function_exists('hl_preliminary_exit')) {
