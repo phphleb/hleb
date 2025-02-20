2501da1e2078b8d35d59b35a414f203f61c64ca3
Foma Tuturov <fomiash@yandex.ru>
2025-02-20 20:33:58 +0300

    SHORE: Debug panel updated

--- vendor/phphleb/debugpan/Controllers/AppController.php
+++ vendor/phphleb/debugpan/Controllers/AppController.php
@@ -20,11 +20,11 @@ class AppController
 
         if ($command === 'help') {
             $content = PHP_EOL .
-            ' --help or -h          (displays a list of default console actions)' . PHP_EOL .
-            ' --version or -v            (displays the version of the framework)' . PHP_EOL .
-            ' --ping            (service health check, returns a constant value)' . PHP_EOL .
-            ' --log-level                    (displays the active logging level)' . PHP_EOL .
-            ' --routes or -r                            (forms a list of routes)' . PHP_EOL;
+                ' --help or -h          (displays a list of default console actions)' . PHP_EOL .
+                ' --version or -v            (displays the version of the framework)' . PHP_EOL .
+                ' --ping            (service health check, returns a constant value)' . PHP_EOL .
+                ' --log-level                    (displays the active logging level)' . PHP_EOL .
+                ' --routes or -r                            (forms a list of routes)' . PHP_EOL;
 
             return $this->getSuccessfulResponse(['data' => $content]);
         }
@@ -32,7 +32,7 @@ class AppController
         if ($command === 'ping') {
             return $this->getSuccessfulResponse(['data' => 'PONG']);
         }
-        
+
         if ($command === 'version') {
             return $this->getSuccessfulResponse(['data' => System::getHlebVersionAsConsoleFormat()]);
         }
@@ -57,4 +57,4 @@ class AppController
     {
         return $this->getSuccessfulResponse(['data' => '(!) Data is not available if debug mode is disabled or partially enabled.']);
     }
- }
+}
