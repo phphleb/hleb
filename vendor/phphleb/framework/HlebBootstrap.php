17ee249f81aa5e646482ae32d58b47c9731cf5e2
Foma <fomiash@yandex.ru>
2025-12-05 01:19:05 +0300

    CHORE: new version

--- vendor/phphleb/framework/HlebBootstrap.php
+++ vendor/phphleb/framework/HlebBootstrap.php
@@ -109,7 +109,7 @@ class HlebBootstrap
 
         // The current version of the framework.
         // Текущая версия фреймворка.
-        \defined('HLEB_CORE_VERSION') or \define('HLEB_CORE_VERSION', '2.1.19');
+        \defined('HLEB_CORE_VERSION') or \define('HLEB_CORE_VERSION', '2.1.20');
 
         $this->logger = $logger;
 
